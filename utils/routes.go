272c18ee3f3224940b208cd0bc6d8b4f8af25f15
Nikhil Kumar <nikhil4040k@gmail.com>
2025-03-27 00:09:31 +0530

    Auth added and everything

--- utils/routes.go
+++ utils/routes.go
@@ -1,15 +1,83 @@
 package utils
 
 import (
+	"errors"
 	"main/events"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func RegisterRoutes(server *gin.Engine) {
 	authenticated := server.Group("/")
+	authenticated.Use(Authenticate)
 	authenticated.GET("/GetAuthors", events.GetAuthors)
 	authenticated.POST("/createauthor", events.CreateAuthors)
 	authenticated.POST("/deleteauthor", events.DeleteAuthors)
 	authenticated.POST("/createstories", events.CreateStory)
+}
+
+const Secretkey = "totallsecretkeylol"
+
+func Generatetoken(email string, userid int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"Email":  email,
+		"userid": userid,
+		"exp":    time.Now().Add(time.Hour * 20).Unix(),
+	})
+	return token.SignedString([]byte(Secretkey))
+}
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.AbortWithStatus(http.StatusNonAuthoritativeInfo)
+		return
+	}
+	err, userid := VerifyToken(token)
+	if err != nil {
+		context.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	context.Set("userid", userid)
+	context.Next()
+
+}
+
+func VerifyToken(token string) (error, int64) {
+	tokenparsed, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Wrong token")
+		}
+		return []byte(Secretkey), nil
+	})
+	if err != nil {
+		return errors.New("couldn't parse"), 0
+	}
+	validornot := tokenparsed.Valid
+	if !validornot {
+		return errors.New("the token is not valid or just expired"), 0
+	}
+	claims, ok := tokenparsed.Claims.(jwt.MapClaims)
+	if !ok {
+		return errors.New("Invalid Claims"), 0
+	}
+	useridRaw, exists := claims["userid"]
+	if !exists {
+		return errors.New("userid not found in claims"), 0
+	}
+	var Userid int64
+	switch v := useridRaw.(type) {
+	case float64:
+		Userid = int64(v)
+	case int64:
+		Userid = v
+	default:
+		return errors.New("unexpected type for userid in claims"), 0
+	}
+
+	return nil, Userid
 }
